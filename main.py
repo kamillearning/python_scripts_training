08a1bc7652f6fe0e87d8656f6a20cebf1d9b5a60
Kamil Tabaka <kamillearning94@gmail.com>
2025-04-21 02:16:20 +0200

    Add functions.py file, move .md files to Docs folder, add first function.
    File cleanup and created first 'Array Function' which is used in main.py.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # This is main.py file, where the scripts will be run.
+from functions import *
+
+# The user enters from what to what value the array is to be created. Then, the function divides the main array into 2 other arrays, which are divided into even and odd.
+number_from = int(input("Specify the value from which you want to create an array: "))
+number_to = int(input("Specify the value on which you want to end the array: "))
+print("Let's divide the entered values into even and odd arrays.")
+
+# We returned 2 divided arrays on which we can perform further operations.
+even, odd = split_numbers_to_arrays(number_from, number_to)
+
+# Basic operations to check operation
+print(f"Added values from an even array: {sum(even)}") 
+print(f"Added values from an odd array: {sum(odd)}") 
